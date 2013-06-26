940eb3ee842a5f7f1a2d7eab1c9dfd1c96a7d800
Alfred Nutile <alfrednutile@gmail.com>
2013-06-25 20:27:54 -0400

    About page is done

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,13 +3,6 @@
 var textMe = jQuery('head title').text().substr(0,5);
 jQuery('#row-text').append('<div id="text-me" class="span9">' + textMe + '</div>');
 
-//Latest Tweat
-
-//$.getJSON("http://api.twitter.com/1/statuses/user_timeline/alnutile.json?count=1&include_rts=1&callback=?", function(data) {
-// var tweat = data[0].text;
-// $("#row-tweat").html('<p class="muted">twitter: ' + tweat + '</p>').fadeIn('slow');
-//});
-
 $("body").fadeIn();
 
 var scrollTop = function() {
@@ -19,4 +12,17 @@ var scrollTop = function() {
 var updateUrl = function(urlGet) {
 	  $.address.state('/');
 	  $.address.value(urlGet); 
+}
+
+
+var replaceURLWithHTMLLinks = function(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp,"<a target='_blank' href='$1'>$1</a>"); 
+}
+
+
+if($('div.sites').length) {
+	var text = $('div.sites').html();
+	var fixed = replaceURLWithHTMLLinks(text);
+	$('div.sites').html(fixed);
 }
