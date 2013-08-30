1109856802a6160ff479f1e4b4df1a713e3c0617
alnutile <alfrednutile@gmail.com>
2013-08-30 05:53:36 -0400

    Remove content sweeper for now

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,8 +1,5 @@
 class ContentController < ApplicationController
-  caches_page :about, :home
-  before_filter { @page_caching = true }
-  cache_sweeper :content_sweeper
-
+  caches_page :about, :home, :support
   respond_to :html
 
   def index
