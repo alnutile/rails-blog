ca86d23f9cdee7d38e0a1c14d80474e01e5dcfc1
alnutile <alfrednutile@gmail.com>
2013-08-30 05:34:02 -0400

    More cache settings

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,4 +1,8 @@
 class ContentController < ApplicationController
+  caches_page :about, :home
+  before_filter { @page_caching = true }
+  cache_sweeper :content_sweeper
+
   respond_to :html
 
   def index
